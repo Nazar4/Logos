a24c5bf5b31b9ebff698192d0ccca0d9d9442c86
Администратор <@MININT-B6UBEH4>
2019-11-22 15:41:21 +0200

    String

---
+++ Logos/src/Homework8/WrongInputConsoleParametersException.java
@@ -0,0 +1,8 @@
+package Homework8;
+
+public class WrongInputConsoleParametersException extends Exception{
+
+    public WrongInputConsoleParametersException(String message) {
+        super(message);
+    }
+}
