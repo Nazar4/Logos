6bce41466aeb2883a1cfa67e785c0829b38718d7
Администратор <@MININT-B6UBEH4>
2019-11-25 16:08:33 +0200

    Exception

--- Logos/src/Homework9/Main.java
+++ Logos/src/Homework9/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
         Methods methods = new Methods();
 
-        //System.out.println(methods.devide(-23,-1312));
-        //System.out.println(methods.minus(23,0));
-        //System.out.println(methods.multiply(0,0));
+        System.out.println(methods.devide(-23,-1312));
+        System.out.println(methods.minus(23,0));
+        System.out.println(methods.multiply(0,0));
         System.out.println(methods.plus(23,78));
 
 
